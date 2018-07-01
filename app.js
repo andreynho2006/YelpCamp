8697b6bfa985fdf9a33ac6b9ab17e094acb3bc18
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-01 20:11:47 +0000

    add bootstrap classes to sstyle the campguards page

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-            {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-            {name: "Granit Hill", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-            {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/eb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104496f0c57bafeab2bb_340.jpg"}
-        ]
+        {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
+        {name: "Granit Hill", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/eb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104496f0c57bafeab2bb_340.jpg"},
+        {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
+        {name: "Granit Hill", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/eb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104496f0c57bafeab2bb_340.jpg"},
+        {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
+        {name: "Granit Hill", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/eb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104496f0c57bafeab2bb_340.jpg"}
+    ];
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -36,4 +42,4 @@ app.get("/campgrounds/new", function(req, res) {
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("YelpCamp Servar Has Started!");
-})
+});
