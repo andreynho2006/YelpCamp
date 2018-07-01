bbc3b179255e65ff70dd5d51442674936af9ecd6
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-01 18:22:03 +0000

    make a new.ejs file to add new campgrounds, add a new route to app.js to campgrounds/new , add body-parser to project

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+            {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
+            {name: "Granit Hill", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
+            {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/eb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104496f0c57bafeab2bb_340.jpg"}
+        ]
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
-            {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-            {name: "Granit Hill", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-            {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/eb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104496f0c57bafeab2bb_340.jpg"}
-        ]
-        
-        res.render("campgrounds", {campgrounds: campgrounds});
-})
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res) {
+    //get data from form add to campground array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampgrounds = {name: name, image: image};
+    campgrounds.push(newCampgrounds);
+    res.redirect("/campgrounds")
+    //redirect to campgrounds page
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs");
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
