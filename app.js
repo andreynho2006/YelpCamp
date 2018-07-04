e0d132721b1d59bb3f4367c6637be9fc06f2bc22
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-04 03:32:03 +0000

    add mongoose to the project, connect the app with the database, make the Schema for campgrounds, make a campground model and create a new campground on server start

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//SCHEMA SETUP
+var campgroundSchema =new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create({
+    name: "Salmon Creek", 
+    image: "https://cdn.vox-cdn.com/thumbor/-JoPdcgAuLTUsWiDZ62CX4wb33k=/0x0:5225x3479/1200x800/filters:focal(2195x1322:3031x2158)/cdn.vox-cdn.com/uploads/chorus_image/image/54137643/camping_tents.0.jpg"
+    }, function(err, campground) {
+        if(err) {
+            console.lo(err);
+        } else {
+            console.log("NEW Campground created");
+            console.log(campground);
+        }
+    });
+
 var campgrounds = [
-        {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-        {name: "Granit Hill", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/eb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104496f0c57bafeab2bb_340.jpg"},
-        {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-        {name: "Granit Hill", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/eb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104496f0c57bafeab2bb_340.jpg"},
-        {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-        {name: "Granit Hill", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f4c371a0eab6_340.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/eb32b7072df5043ed1584d05fb1d4e97e07ee3d21cac104496f0c57bafeab2bb_340.jpg"}
+        {name: "Granit Hill", image: "http://4.bp.blogspot.com/-0xecSPGE3fc/UnYi6NjL_AI/AAAAAAAAClw/hzeZky6mIrQ/s1600/DSC05653.JPG"},
+        {name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsmXnA0nPXU41uscxsnX1loXVu9ddWJ2NrqS8UeOMTyySFLrUk"},
+        {name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7m0WfJxvQvAZY09mFtZ9F1QxpLAPS3-dROt684wAABdLIGrwB"},
+        {name: "Granit Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD9ZYROUpiiOIDwL3PPcqGkIQCvsEE1VrZUWRz8vT0cERillht"},
+        {name: "Mountain Goat's Rest", image: "http://www.editionsblabla.fr/wp-content/uploads/2018/02/location-camping-pont-du-tarn-coco-sweet-min.jpg"},
+        {name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYTLn-GXTBdOTezWA9cwF24B_lkk7Xh2sT-2SIPmQaUSw4mgvT"},
+        {name: "Granit Hill", image: "https://www.quebecoriginal.com/en/listing/images/800x600/ae2894cf-af0a-46dc-904d-8a91b0059376/camping-parc-national-du-mont-tremblant-de-la-diable-camping-secteur-la-diable.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://cdn.vox-cdn.com/thumbor/G06ar5iSYGeKFUNi5Ubfji7b_V4=/0x0:3968x2976/1200x800/filters:focal(1667x1171:2301x1805)/cdn.vox-cdn.com/uploads/chorus_image/image/57016269/170817_GT_TIPI_01.0.jpg"}
     ];
 
 app.get("/", function(req, res) {
