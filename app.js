0ffc406a41619607ea16cefd9caccd53a31acdca
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-07 03:29:37 +0000

    make a models dir and a campground file and move there the Schema

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-//SCHEMA SETUP
-var campgroundSchema =new mongoose.Schema({
-    name: String,
-    image: String, 
-    description: String
-});
 
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
 //     name: "Granit Hill", 
