e5476182b0669f9b7825573d6b49a8cdb3504d53
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-11 05:10:03 +0000

    add author id and author username to comment and save it, display the name of the user when add a new comment, and show it automatically when a new comment is added

--- routes/comments.js
+++ routes/comments.js
@@ -29,6 +29,11 @@ router.post("/", isLoggedIn, function(req, res) {
                 if(err) {
                     console.log(err);
                 } else {
+                    //add username and id to comments
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    //save comment
+                    comment.save();
                     //connect new comment to campground
                     campground.comments.push(comment);
                     campground.save();
