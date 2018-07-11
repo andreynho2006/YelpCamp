905fb464b54925240f98e4df643989ab4e4e381a
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-11 04:55:28 +0000

    add id with type and ref and username to the commentSchema, comment seedDB() and remove all campgrounds posts from DB

--- seeds.js
+++ seeds.js
@@ -22,34 +22,34 @@ function seedDB(){
     
     //Remove all campgrounds
     Campground.remove({}, function(err) {
-        if(err) {
-            console.log(err);
-        }
-        console.log("Remove campgrounds!");
-        //add a few campgrounds
-        data.forEach(function(seed) {
-            Campground.create(seed, function(err, campground) {
-                if(err) {
-                    console.log(err);
-                } else {
-                    console.log("Added data");
-                    //create a comment
-                    Comment.create({
-                        text: "this place is great, but I wish there was internet",
-                        author: "Andrei Cirlan"
-                    }, function(err, comment){
-                       if(err) {
-                           console.log(err);
-                       } else {
-                           campground.comments.push(comment);
-                           campground.save();
-                           console.log("created new comment");
-                       }
-                    });
+        // if(err) {
+        //     console.log(err);
+        // }
+        // console.log("Remove campgrounds!");
+        // //add a few campgrounds
+        // data.forEach(function(seed) {
+        //     Campground.create(seed, function(err, campground) {
+        //         if(err) {
+        //             console.log(err);
+        //         } else {
+        //             console.log("Added data");
+        //             //create a comment
+        //             Comment.create({
+        //                 text: "this place is great, but I wish there was internet",
+        //                 author: "Andrei Cirlan"
+        //             }, function(err, comment){
+        //               if(err) {
+        //                   console.log(err);
+        //               } else {
+        //                   campground.comments.push(comment);
+        //                   campground.save();
+        //                   console.log("created new comment");
+        //               }
+        //             });
                     
-                }
-            });
-        });
+        //         }
+        //     });
+        // });
     });
     
     //add a few comments
