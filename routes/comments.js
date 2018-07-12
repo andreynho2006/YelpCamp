9e241b4a63ed9d6cfec86d6e0fb167cfc57df819
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-12 04:16:43 +0000

    edit the route for comments and add edit button for comment

--- routes/comments.js
+++ routes/comments.js
@@ -45,6 +45,16 @@ router.post("/", isLoggedIn, function(req, res) {
     });
 });
 
+router.get("/:comment_id/edit", function(req, res) {
+    Comment.findById(req.params.comment._id, function(err, foundComment) {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
