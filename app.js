207ef6a89d1c7f3cecc21918f82aa824a5218f81
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-25 15:06:20 +0000

    add nwe database mongolab db=yelpcampandrei

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://andrei:Floraria1@ds247061.mlab.com:47061/yelpcampandrei");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
